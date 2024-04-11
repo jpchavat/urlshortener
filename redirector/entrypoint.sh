a4af8f24b85bdc8f4610949ba7e7b68e5f5d76f4
JP Chavat <jpchavat@gmail.com>
2024-04-11 06:22:28 -0300

    Fix little bug in the entrypoint sh

--- redirector/entrypoint.sh
+++ redirector/entrypoint.sh
@@ -1,4 +1,4 @@
 #CMD ["python", "common/generate_db.py", "&&", "gunicorn", "-w", "2", "-b", "0.0.0.0:8000", "--access-logfile=-", "admin.app:create_app()"]
-pyhton common/generate_db.py;
+pyhton3 common/generate_db.py;
 gunicorn -w 2 -b 0.0.0.0:8080 --access-logfile=- redirector.app:create_app\(\);
 
