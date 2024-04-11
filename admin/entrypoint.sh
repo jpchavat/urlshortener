dcab3b21751a67d26d501b196443d89da1267a35
JP Chavat <jpchavat@gmail.com>
2024-04-11 06:33:14 -0300

    python3.9:slim don't find python in itself... seems buggy. just used the abs path to python as workaround

--- admin/entrypoint.sh
+++ admin/entrypoint.sh
@@ -1,4 +1,5 @@
 #CMD ["python", "common/generate_db.py", "&&", "gunicorn", "-w", "2", "-b", "0.0.0.0:8000", "--access-logfile=-", "admin.app:create_app()"]
-pyhton3 common/generate_db.py;
+sleep 5;
+/usr/local/bin/python generate_db.py;
 gunicorn -w 2 -b 0.0.0.0:8000 --access-logfile=- admin.app:create_app\(\);
 
