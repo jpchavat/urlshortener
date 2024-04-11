0b8b2ba6c7f369bc02fef29ccd97995108be4896
JP Chavat <jpchavat@gmail.com>
2024-04-11 06:35:29 -0300

    fix path error

--- redirector/entrypoint.sh
+++ redirector/entrypoint.sh
@@ -1,5 +1,5 @@
 #CMD ["python", "common/generate_db.py", "&&", "gunicorn", "-w", "2", "-b", "0.0.0.0:8000", "--access-logfile=-", "admin.app:create_app()"]
 sleep 5;
-/usr/local/bin/python generate_db.py;
+/usr/local/bin/python common/generate_db.py;
 gunicorn -w 2 -b 0.0.0.0:8080 --access-logfile=- redirector.app:create_app\(\);
 
